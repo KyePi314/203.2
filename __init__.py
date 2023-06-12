64651359c73ffd54b5ac8081b19df4251275b09a
KyePi314 <106054184+KyePi314@users.noreply.github.com>
2023-06-12 18:15:19 +1200

    connected signup/login to database

--- __init__.py
+++ __init__.py
@@ -1,7 +1,14 @@
 ## The init file ##
 from flask import Flask, render_template, request, Blueprint
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate, migrate
+import os
+SECRET_KEY = os.urandom(32)
 
+
+login_manager = LoginManager()
+migrate = Migrate()
 # #Create the Database
 # engine = create_engine("sqlite:///database.db", echo=True)
 # #Uses the engine to create the tables for data.
@@ -45,3 +52,21 @@ from flask_sqlalchemy import SQLAlchemy
 #              "TimelineBox1: " : "..."}
 
 # The function that initializes the application and its authentication system/databse 
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = SECRET_KEY
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+    from models import engine
+    migrate.init_app(app, engine)
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    
+    # Registering the blueprint for the auth routes in the site
+    from authentication import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+    #Registering the main routes used in the site
+    from main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    return app
--- main.py
+++ main.py
@@ -3,13 +3,10 @@ from flask import Flask, render_template, request, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_required, current_user, LoginManager
 from flask_login import UserMixin
-from authentication import auth
-from flask_migrate import Migrate, migrate
-import os
-SECRET_KEY = os.urandom(32)
+from __init__ import create_app
 
-login_manager = LoginManager()
-migrate = Migrate()
+
+
 main = Blueprint('main', __name__)
 
 
@@ -53,42 +50,10 @@ def worldInfo():
 def userprofile():
     return render_template("userProfile.html")
 
-
-from models import db, User
-
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = SECRET_KEY
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-    db.init_app(app)
-    migrate.init_app(app, db)
-    
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-    
-    # Registering the blueprint for the auth routes in the site
-    from authentication import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-    #Registering the main routes used in the site
-    from main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-    return app
-
-
-
-
 app = create_app()
 
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-with app.app_context():
-    db.create_all()
 
 if __name__ == '__203.main__':
     app.run(debug=True)
